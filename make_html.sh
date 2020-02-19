012233afbeb6cf911683e6f49dd7ea43e07574d8
mdulac <melchior.dulac@inra.fr>
2020-02-19 18:28:37 +0100

    first

---
+++ make_html.sh
@@ -0,0 +1,53 @@
+mkdir ~/workspace/Galaxy-SynBioCAD/html/
+#RetroPath2.0
+cd ~/workspace/Galaxy-SynBioCAD/RetroPath2/RetroPath2_image/
+node ~/workspace/markdown-to-html-github-style/convert.js RetroPath2
+mv README.html ~/workspace/Galaxy-SynBioCAD/html/RetroPath2_galaxyTool.hml
+#rp2paths
+cd ~/workspace/Galaxy-SynBioCAD/rp2paths/rp2paths_image/
+node ~/workspace/markdown-to-html-github-style/convert.js rp2paths
+mv README.html ~/workspace/Galaxy-SynBioCAD/html/rp2paths_galaxyTool.html
+#rpReader
+cd ~/workspace/Galaxy-SynBioCAD/rpReader/rpReader_image/
+node ~/workspace/markdown-to-html-github-style/convert.js rpReader
+mv README.html ~/workspace/Galaxy-SynBioCAD/html/rpReader_galaxyTool.html
+#rpCofactors
+cd ~/workspace/Galaxy-SynBioCAD/rpCofactors/rpCofactors_image/
+node ~/workspace/markdown-to-html-github-style/convert.js rpCofactors
+mv README.html ~/workspace/Galaxy-SynBioCAD/html/rpCofactors_galaxyTool.html
+#rpFBA
+cd ~/workspace/Galaxy-SynBioCAD/rpFBA/rpFBA_image/
+node ~/workspace/markdown-to-html-github-style/convert.js rpFBA
+mv README.html ~/workspace/Galaxy-SynBioCAD/html/rpFBA_galaxyTool.html
+#rpThermo
+cd ~/workspace/Galaxy-SynBioCAD/rpThermo/rpThermo_image/
+node ~/workspace/markdown-to-html-github-style/convert.js rpThermo
+mv README.html ~/workspace/Galaxy-SynBioCAD/html/rpThermo_galaxyTool.html
+#rpSelenzyme
+cd ~/workspace/Galaxy-SynBioCAD/rpSelenzyme/rpSelenzyme_image/
+node ~/workspace/markdown-to-html-github-style/convert.js rpSelenzyme
+mv README.html ~/workspace/Galaxy-SynBioCAD/html/rpSelenzyme_galaxyTool.html
+#rpVisualiser
+cd ~/workspace/Galaxy-SynBioCAD/rpVisualiser/rpVisualiser_image/
+node ~/workspace/markdown-to-html-github-style/convert.js rpVisualiser
+mv README.html ~/workspace/Galaxy-SynBioCAD/html/rpVisualiser_galaxyTool.html
+#rpReport
+cd ~/workspace/Galaxy-SynBioCAD/rpReport/rpReport_image/
+node ~/workspace/markdown-to-html-github-style/convert.js rpReport
+mv README.html ~/workspace/Galaxy-SynBioCAD/html/rpReport_galaxyTool.html
+#rpGlobalScore
+cd ~/workspace/Galaxy-SynBioCAD/rpGlobalScore/rpGlobalScore_image/
+node ~/workspace/markdown-to-html-github-style/convert.js rpGlobalScore
+mv README.html ~/workspace/Galaxy-SynBioCAD/html/rpGlobalScore_galaxyTool.html
+#rpMakeSource
+cd ~/workspace/Galaxy-SynBioCAD/rpMakeSource/rpMakeSource_image/
+node ~/workspace/markdown-to-html-github-style/convert.js rpMakeSoure
+mv README.html ~/workspace/Galaxy-SynBioCAD/html/rpMakeSource_galaxyTool.html
+#rpExtractSink
+cd ~/workspace/Galaxy-SynBioCAD/rpExtractSink/rpExtractSink_image/
+node ~/workspace/markdown-to-html-github-style/convert.js rpExtractSink
+mv README.html ~/workspace/Galaxy-SynBioCAD/html/rpExtractSink_galaxyTool.html
+#rpSBMLtoSBOL
+cd ~/workspace/Galaxy-SynBioCAD/rpSBMLtoSBOL/rpSBMLtoSBOL_image/
+node ~/workspace/markdown-to-html-github-style/convert.js rpSBMLtoSBOL
+mv README.html ~/workspace/Galaxy-SynBioCAD/html/rpSBMLtoSBOL_galaxyTool.html
